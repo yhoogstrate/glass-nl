57b4016bcdf6ccb3c42648ed3964a729f4bc72ab
yhoogstrate <y.hoogstrate@gmail.com>
2022-03-11 17:23:57 +0100

    estimate purity correlation

---
+++ scripts/load_correlation_expression_purity.R
@@ -0,0 +1,30 @@
+#!/usr/bin/env R 
+
+# load libs ----
+
+
+# load data ----
+
+if(!exists('expression.glass.vst')) {
+  source('scripts/load_rna-counts.R')
+}
+
+if(!exists("metadata.glass.per.resection") | "dna.wes.VAF_IDH" %in% names(metadata.glass.per.resection) == F) {
+  source('scripts/load_tumour_purities.R')
+}
+
+
+# calc corr for ACE method ----
+
+tmp.metadata <- metadata.glass.per.resection %>%
+  dplyr::filter(excluded == F) %>% 
+  dplyr::filter(!is.na(dna.shallow.ACE.purity))
+
+tmp.data <- expression.glass.vst %>% 
+  dplyr::select(tmp.metadata$genomescan.sid)
+
+
+stopifnot(tmp.metadata$genomescan.sid == colnames(tmp.data))
+
+
+cor.test(tmp.metadata$dna.shallow.ACE.purity, as.numeric(tmp.data[1,]))
