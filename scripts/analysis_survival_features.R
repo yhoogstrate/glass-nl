b8dac4a403e8dfe24aa8e9e2e938d24212b43211
yhoogstrate <y.hoogstrate@gmail.com>
2022-03-17 11:29:03 +0100

    sav

--- scripts/analysis_survival_features.R
+++ scripts/analysis_survival_features.R
@@ -43,16 +43,42 @@ tmp.metadata <- metadata.glass.per.patient %>%
   dplyr::select(genomescan.sid.R) %>% 
   dplyr::left_join(metadata.glass.per.resection, by=c('genomescan.sid.R'='genomescan.sid')) %>%
   tidyr::drop_na(time.resection.until.last.event) %>% 
-  dplyr::arrange(Sample_Type, genomescan.sid.R)
+  dplyr::rename(genomescan.sid = genomescan.sid.R) %>% 
+  dplyr::arrange(Sample_Type, genomescan.sid)
 
 
 
-tmp.data <- expression.glass %>%
-  dplyr::select(all_of( tmp.metadata$genomescan.sid.R ))
+tmp.data <- expression.glass.vst %>%
+  dplyr::select(all_of( tmp.metadata$genomescan.sid )) %>% 
+  dplyr::mutate(mad =  apply( as.matrix(.), 1, stats::mad) ) %>% 
+  dplyr::arrange(mad) %>% 
+  dplyr::mutate(mad=NULL) 
 
 
 
 stopifnot(colnames(tmp.data) == tmp.metadata$genomescan.sid)
+
+
+
+svvl.obj <- tmp.metadata %>%
+  dplyr::rename(svvl = time.resection.until.last.event) %>% 
+  dplyr::rename(status = status.resection.until.last.event) %>%
+  dplyr::select(genomescan.sid, svvl, status) %>% 
+  dplyr::left_join(
+    tmp.data %>%
+      dplyr::slice_head(n=150) %>% 
+      t %>%
+      as.data.frame(stringsAsFactors=F) %>% 
+      tibble::rownames_to_column('genomescan.sid'),
+    by=c('genomescan.sid'='genomescan.sid')
+  ) %>% 
+  tibble::column_to_rownames('genomescan.sid') %>% 
+  dplyr::mutate(svvl = as.numeric(svvl))
+
+
+
+pbc.obj <- rfsrc(Surv(svvl,status) ~ ., svvl.obj , importance = TRUE,ntree=1000)
+plot(pbc.obj)
 
 
 
