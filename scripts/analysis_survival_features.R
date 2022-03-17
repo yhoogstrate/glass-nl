3f84eaf83ba5f4dc5684aafcad1453c4145d00af
yhoogstrate <y.hoogstrate@gmail.com>
2022-03-17 15:00:29 +0100

    update

--- scripts/analysis_survival_features.R
+++ scripts/analysis_survival_features.R
@@ -1,6 +1,15 @@
 #!/usr/bin/env R 
 
 
+# load libs ----
+
+
+library(randomForestSRC)# https://cran.r-project.org/web/packages/randomForestSRC/index.html
+library(survival)
+library(RegParallel)
+
+
+
 # load data ----
 
 
@@ -15,13 +24,6 @@ if(!exists("expression.glass.vst")) {
   source('scripts/load_rna-counts.R')
 }
 
-# load libs ----
-
-
-# https://cran.r-project.org/web/packages/randomForestSRC/index.html
-library(randomForestSRC)
-library(survival)
-
 
 # example RF model ----
 
@@ -35,10 +37,7 @@ pbc.obj <- rfsrc(Surv(days,status) ~ ., pbc, importance = TRUE)
 plot(pbc.obj)
 
 
-# R2 svvl RF ----
-
-# Van alle resecties tijd tot dood of laatste event nodig
-
+# R2 svvl ----
 
 
 tmp.metadata <- metadata.glass.per.patient %>%
@@ -69,7 +68,7 @@ svvl.obj <- tmp.metadata %>%
   dplyr::select(genomescan.sid, svvl, status) %>% 
   dplyr::left_join(
     tmp.data %>%
-      dplyr::slice_head(n=500) %>% 
+      dplyr::slice_head(n=10000) %>% 
       t %>%
       as.data.frame(stringsAsFactors=F) %>% 
       tibble::rownames_to_column('genomescan.sid'),
@@ -79,12 +78,12 @@ svvl.obj <- tmp.metadata %>%
   dplyr::mutate(svvl = as.numeric(svvl))
 
 
+## RF (RFSRC) ----
 
-pbc.obj <- rfsrc(Surv(svvl,status) ~ ., svvl.obj , importance = TRUE,ntree=5000)
+
+pbc.obj <- rfsrc(Surv(svvl,status) ~ ., svvl.obj , importance = TRUE,ntree=4000,seed=1234)
 plot.rfsrc(pbc.obj)
-
-
-plot.variable.rfsrc(pbc.obj)
+# plot.variable.rfsrc(pbc.obj)
 
 
 plt <- data.frame(pbc.obj$importance) %>% 
@@ -94,12 +93,104 @@ plt <- data.frame(pbc.obj$importance) %>%
   tibble::rownames_to_column('gene_uid') %>% 
   dplyr::mutate(baseline = 0) %>% 
   dplyr::mutate(y = rank(pbc.obj.importance)) %>% 
-  tidyr::pivot_longer(cols = -c(gene_uid, y))
+  tidyr::pivot_longer(cols = -c(gene_uid, y)) %>% 
+  dplyr::mutate(name=NULL) %>% 
+  dplyr::rename(RFSRC.importance = value)
 
-
-ggplot(plt, aes(x=value,y=reorder(gene_uid, y), group=gene_uid)) +
+ggplot(plt, aes(x=RFSRC.importance,y=reorder(gene_uid, y), group=gene_uid)) +
   geom_line(lwd=2) +
-  youri_gg_theme
+  youri_gg_theme +
+  labs(y=NULL)
+
+
+
+
+# R1 svvl ----
+
+# Van alle resecties tijd tot dood of laatste event nodig
+
+
+
+tmp.metadata <- metadata.glass.per.patient %>%
+  tidyr::drop_na(genomescan.sid.I) %>%
+  dplyr::select(genomescan.sid.I) %>% 
+  dplyr::left_join(metadata.glass.per.resection, by=c('genomescan.sid.I'='genomescan.sid')) %>%
+  tidyr::drop_na(time.resection.until.last.event) %>% 
+  dplyr::rename(genomescan.sid = genomescan.sid.I) %>% 
+  dplyr::arrange(Sample_Type, genomescan.sid)
+
+
+
+tmp.data <- expression.glass.vst %>%
+  dplyr::select(all_of( tmp.metadata$genomescan.sid )) %>% 
+  dplyr::mutate(mad =  apply( as.matrix(.), 1, stats::mad) ) %>% 
+  dplyr::arrange(mad) %>% 
+  dplyr::mutate(mad=NULL) 
+
+
+
+stopifnot(colnames(tmp.data) == tmp.metadata$genomescan.sid)
+
+
+
+svvl.obj <- tmp.metadata %>%
+  dplyr::rename(svvl = time.resection.until.last.event) %>% 
+  dplyr::rename(status = status.resection.until.last.event) %>%
+  dplyr::select(genomescan.sid, svvl, status) %>% 
+  dplyr::left_join(
+    tmp.data %>%
+      dplyr::slice_head(n=10000) %>% 
+      t %>%
+      as.data.frame(stringsAsFactors=F) %>% 
+      tibble::rownames_to_column('genomescan.sid'),
+    by=c('genomescan.sid'='genomescan.sid')
+  ) %>% 
+  tibble::column_to_rownames('genomescan.sid') %>% 
+  dplyr::mutate(svvl = as.numeric(svvl))
+
+
+## RF (RFSRC) ----
+
+
+pbc.obj <- rfsrc(Surv(svvl,status) ~ ., svvl.obj , importance = TRUE,ntree=4000,seed=1234)
+plot.rfsrc(pbc.obj)
+# plot.variable.rfsrc(pbc.obj)
+
+
+plt <- data.frame(pbc.obj$importance) %>% 
+  dplyr::arrange(abs(pbc.obj.importance)) %>% 
+  dplyr::top_n(20) %>% 
+  dplyr::arrange(-pbc.obj.importance) %>% 
+  tibble::rownames_to_column('gene_uid') %>% 
+  dplyr::mutate(baseline = 0) %>% 
+  dplyr::mutate(y = rank(pbc.obj.importance)) %>% 
+  tidyr::pivot_longer(cols = -c(gene_uid, y)) %>% 
+  dplyr::mutate(name=NULL) %>% 
+  dplyr::rename(RFSRC.importance = value)
+
+ggplot(plt, aes(x=RFSRC.importance,y=reorder(gene_uid, y), group=gene_uid)) +
+  geom_line(lwd=2) +
+  youri_gg_theme +
+  labs(y=NULL)
+
+
+## Coxph ----
+
+coxph.res <- RegParallel(
+  data = svvl.obj ,
+  formula = 'Surv(svvl, status) ~ [*]',
+  FUN = function(formula, data)
+    coxph(formula = formula,
+          data = data,
+          ties = 'breslow',
+          singular.ok = TRUE),
+  FUNtype = 'coxph',
+  variables = colnames(coxdata)[3:ncol(coxdata)],
+  blocksize = 65,
+  cores = 2,
+  nestedParallel = FALSE,
+  conflevel = 95)
+
 
 
 
