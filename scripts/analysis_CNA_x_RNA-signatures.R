9cd50ff2338576c82325c5a9af82cf597d338f0c
yhoogstrate <y.hoogstrate@gmail.com>
2022-06-22 14:01:06 +0200

    update

--- scripts/analysis_CNA_x_RNA-signatures.R
+++ scripts/analysis_CNA_x_RNA-signatures.R
@@ -2,9 +2,14 @@
 
 # see if the CNAs can explain (some part of) the expression of the DGE signatures
 
+
+# load data ----
+
+
 if(!exists('cnv') | !exists('cnv.metadata')) {
   source('scripts/load_genomic_alterations.R')
 }
+
 
 
 
